8d709679c279d45cf45ffa2b9e15947d669b9c93
罗成 <hx0010913@hengxinyongli.com>
2018-08-16 22:25:07 +0800

    详情页面

--- src/js/detail.js
+++ src/js/detail.js
@@ -7,4 +7,13 @@ $(function(){
             $('.pages-con').removeClass("active");
         }
       );
+    $(window).scroll( function() {  // 滚动监听
+        debouce((function(){ // 
+            if($(window).scrollTop() > 900) {
+                $('.detail-navbar').addClass('affix');
+            } else {
+                $('.detail-navbar').removeClass('affix');
+            }
+        })(),300);
+    })
 })
